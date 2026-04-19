3f0fac512ab40627cc0dd953a8018da9250721c3
krnuthannuthu <krnuthannuthu@gmail.com>
2026-04-19 15:24:00 +0530

    upload lab 7

---
+++ Lab-7a/FirstServlet.java
@@ -0,0 +1,36 @@
+package com.example;
+//Lab 7a Execute the following Hello World ! Servlet Worked out Example
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloWorldSecond")
+public class FirstServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    public FirstServlet () {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html><body>");
+        out.println("<h2 style='color:blue;'>Hello World!!!</h2>");
+        out.println("<p style='color:magenta;'>The server time is: " + new java.util.Date() + "</p>");
+        out.println("</body></html>");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
